4ac8885f85f25c34ab5add57a8f79460a29e9985
Ajit Jadhav <mr.ajitjadhav@gmail.com>
2018-08-17 22:36:29 +0530

    Previous and Next lec controls added

--- resize.js
+++ resize.js
@@ -1,17 +1,57 @@
+let lastUpdatedQuery = "#region-main .modified"
+let lastUpdatedElement = document.querySelector(lastUpdatedQuery)
+let activeNodeClass = "active_tree_node"
+let listClass = "type_activity depth_5"
+let iframeQuery = "#region-main .no-overflow iframe"
 
-window.onload = function(event){
-    resizeIframe()
-    window.onresize = function(event) {
-        resizeIframe()        
+let controlsNode = document.createElement('span')
+controlsNode.style.float = 'right'
+
+if(lastUpdatedElement)
+    lastUpdatedElement.appendChild(controlsNode)
+
+window.onload = (event) => {
+    if(document.querySelector(iframeQuery)) {
+        resizeIframe()
+        putControls()
+        window.onresize = (event) => {
+            resizeIframe()        
+        }
     }
 }
 
-var resizeIframe = function(){
-    if( window.window.innerHeight < window.window.innerWidth) {
-        document.querySelector("#region-main .no-overflow iframe").width = "95%"  
-    } else {
-        document.querySelector("#region-main .no-overflow iframe").width = "100%"
+function putControls() {
+    let activeNode = document.getElementsByClassName(activeNodeClass)[0].parentElement
+    if(activeNode.className.match(/.*type_activity.*depth_5.*/g)) {
+        let prevHref = activeNode.previousSibling ? activeNode.previousSibling.firstChild.firstChild.href : false
+
+        let nextHref = activeNode.nextSibling ? activeNode.nextSibling.firstChild.firstChild.href : false
+
+        if(prevHref) {
+            let prevElm = document.createElement('a')
+            prevElm.setAttribute('href', prevHref)
+            prevElm.innerHTML = "&nbspPrev&nbsp"
+            controlsNode.appendChild(prevElm)
+            
+            let blankElm = document.createElement('span')
+            blankElm.innerHTML = '&nbsp;&nbsp;'
+            controlsNode.appendChild(blankElm)
+        }
+        if(nextHref) {
+            let nextElm = document.createElement('a')
+            nextElm.setAttribute('href', nextHref)
+            nextElm.innerHTML = "&nbspNext&nbsp"
+            controlsNode.appendChild(nextElm)
+        }
     }
-    currentIframeWidth = parseInt(document.querySelector("#region-main .no-overflow iframe").clientWidth) 
-    document.querySelector("#region-main .no-overflow iframe").height = currentIframeWidth * 0.562
+}
+
+function resizeIframe() {
+    if( window.window.innerHeight < window.window.innerWidth) {
+        document.querySelector(iframeQuery).width = "95%"  
+    } else {
+        document.querySelector(iframeQuery).width = "100%"
+    }
+    currentIframeWidth = parseInt(document.querySelector(iframeQuery).clientWidth) 
+    document.querySelector(iframeQuery).height = currentIframeWidth * 0.562
 }
